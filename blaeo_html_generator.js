8b6dfdf6a74074c853495f40936e80ad965129eb
jullebarge <jullebarge@users.noreply.github.com>
2018-02-13 11:50:48 +0100

    Change textarea to clickable icon
    Remove the text area and replaced it with an icon that put the Html code in the clipboard.
    Problem to fix: the icon is removed when you click ok on the popup.

--- blaeo_html_generator.js
+++ blaeo_html_generator.js
@@ -1,12 +1,13 @@
 // ==UserScript==
-// @name         BLAEO Game Post Generator
+// @name         BLAEO Game Post Generator with Button
 // @namespace    https://www.backlog-assassins.net/
-// @version      0.2
+// @version      0.3
 // @description  add a button on game list to generate an html post for each game
 // @author       JulLeBarge
 // @match        https://www.backlog-assassins.net/users/*/games
 // @match        https://www.backlog-assassins.net/*/games/*
 // @match        https://www.backlog-assassins.net/lists/*
+// @grant	 GM_setClipboard
 // ==/UserScript==
 
 (function() {
@@ -18,7 +19,7 @@
             return;
         }
 
-		// add icon to each game cell
+	// add icon to each game cell
         var icons = [];
         $('.game-table tbody tr').each(function() {
             var $row = $(this);
@@ -38,22 +39,14 @@
 				apptime = $tds.eq(2).text() + " playtime";
 			}
 
-			var tempAch = appachievements.split("\n");
-			var tempAchPourcent = tempAch[0];
-            var tempAchNb = tempAch[1].slice(1, tempAch[1].indexOf(' '));
-			var tempAchTotal = tempAch[1].slice(tempAch[1].lastIndexOf(' '),tempAch[1].length-1);
-			
-			appachievements = tempAchNb + " of " + tempAchTotal + " achievements (" + tempAchPourcent + ")";
-			/*console.log("tempAchPourcent: " + tempAchPourcent);
-			console.log("tempAchNb: " + tempAchNb);
-			console.log("tempAchTotal: " + tempAchTotal);
-            console.log("appid= " + appid);
-			console.log("appname: " + appname);
-			console.log("apptime: " + apptime);
-			console.log("appachievements: " + appachievements);*/
+		var tempAch = appachievements.split("\n");
+		var tempAchPourcent = tempAch[0];
+            	var tempAchNb = tempAch[1].slice(1, tempAch[1].indexOf(' '));
+		var tempAchTotal = tempAch[1].slice(tempAch[1].lastIndexOf(' '),tempAch[1].length-1);
 
-			var tempHtml = "<li><textarea rows=1 cols=25>";
-            tempHtml += "<div class='panel panel-success'>";
+		appachievements = tempAchNb + " of " + tempAchTotal + " achievements (" + tempAchPourcent + ")";
+
+			var tempHtml = "<div class='panel panel-success'>";
 			tempHtml += "<div class='panel-heading' data-toggle='collapse' data-target='#post-" +  appid + "' aria-expanded='true'>";
 			tempHtml += "<div class='game game-beaten game-media' data-item='data-" + appid + "'>";
 			tempHtml += "<div style='float: left; padding-right: 10px;'>";
@@ -64,9 +57,18 @@
 			tempHtml += "<span>NOTE</span><br>" + apptime + ", <a href='http://steamcommunity.com/id/jullebarge/stats/" +  appid + "/?tab=achievements'>" +  appachievements + "</a>";
 			tempHtml += "</div></div></div><div id='post-" +  appid + "' class='panel-body collapse' aria-expanded='false' style=''>";
 			tempHtml += "<font color='green'><b>Pros:</b></font><br><br><font color='red'><b>Cons:</b></font><br><br><font color='blue'><b>Conclusion:</b></font></div></div>";
-            tempHtml += "</textarea></li>";
+
 			tempHtml = tempHtml.replace(/'/g, '"');
-            var $icon = $(tempHtml).insertAfter($row.find('li:nth-child(2)'));
+
+            var buttonHtml = "<li><a class='fa fa-clipboard' style='text-decoration:none;' id='Button-" +  appid + "' href=''><i></i></a></li>";
+            var $icon = $(buttonHtml).insertAfter($row.find('li:nth-child(2)'));
+
+			document.getElementById ("Button-" +  appid).addEventListener (
+				"click", function () {
+				GM_setClipboard(tempHtml);
+				alert("Html copied in the clipboard");
+				}, false
+			);
             icons.push($icon);
         });
 	};
